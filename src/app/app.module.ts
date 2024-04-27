c58a35ae5a21d200bee593df09879a81d1487498
Majdi-msallem <majdi.msallem@esprit.tn>
2024-04-27 12:19:50 +0200

    component-handson

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FirstComponentComponent } from './first-component/first-component.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstComponentComponent,
+    FooterComponent,
+    HeaderComponent,
+    SidenavComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
